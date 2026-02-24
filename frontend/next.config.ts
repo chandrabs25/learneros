93d7b512d4f5fc5a4be54c768df0f2153a52a919
chandrabs25 <chandrabs25@users.noreply.github.com>
2026-02-24 20:35:42 +0530

    Add concept lineage graph action

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -14,7 +14,8 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.googleapis.com https://static.cloudflareinsights.com",
+              "script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.googleapis.com https://apis.google.com https://static.cloudflareinsights.com",
+              "script-src-elem 'self' 'unsafe-inline' https://www.gstatic.com https://www.googleapis.com https://apis.google.com https://static.cloudflareinsights.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com data:",
               "img-src 'self' data: blob: https:",
